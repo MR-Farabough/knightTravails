4153dfce9da25768f33800afbe35520f4dfab5aa
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-05-26 00:17:00 -0500

    Code Refactor

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
-function knightTravails(startPosition, endPosition) {
-    function possibleSquares(intArray) {
-        let resultsArray = []
-        const calculationArray = [
-        [intArray[0] + 1, intArray[1] + 2],
-        [intArray[0] - 1, intArray[1] + 2],
-        [intArray[0] + 1, intArray[1] - 2],
-        [intArray[0] - 1, intArray[1] - 2],
-        [intArray[0] - 2, intArray[1] + 1],
-        [intArray[0] - 2, intArray[1] - 1],
-        [intArray[0] + 2, intArray[1] + 1],
-        [intArray[0] + 2, intArray[1] - 1]]
-        const tempStorage = [...new Set(calculationArray)]
-        checkAvailablePositions()
-        function checkAvailablePositions() {
-            tempStorage.forEach(positionData => {
-                const checkOne = positionData[0] < 9 && positionData[0] > 0
-                const checkTwo = positionData[1] < 9 && positionData[1] > 0
-                if (checkOne && checkTwo) {
-                    resultsArray.push(positionData)
-                }
-            });
-        }
-        return resultsArray
+function possibleSquares(intArray) {
+    let resultsArray = []
+    const calculationArray = [
+    [intArray[0] + 1, intArray[1] + 2],
+    [intArray[0] - 1, intArray[1] + 2],
+    [intArray[0] + 1, intArray[1] - 2],
+    [intArray[0] - 1, intArray[1] - 2],
+    [intArray[0] - 2, intArray[1] + 1],
+    [intArray[0] - 2, intArray[1] - 1],
+    [intArray[0] + 2, intArray[1] + 1],
+    [intArray[0] + 2, intArray[1] - 1]]
+    const tempStorage = [...new Set(calculationArray)]
+    checkAvailablePositions()
+    function checkAvailablePositions() {
+        tempStorage.forEach(positionData => {
+            const checkOne = positionData[0] < 9 && positionData[0] > 0
+            const checkTwo = positionData[1] < 9 && positionData[1] > 0
+            if (checkOne && checkTwo) {
+                resultsArray.push(positionData)
+            }
+        });
     }
-    const startPosMoves = possibleSquares(startPosition)
-    const endPosMoves = possibleSquares(endPosition)
-    let pathTaken = ''
-    let moveDict = {}
-    endPosMoves.forEach(move => {
-        moveDict[move] = true
-    })
-    startPosMoves.forEach(move => {
-        if (move[0] == endPosition[0] && move[1] == endPosition[1]) {
-            pathTaken = `
-ONE WAY TICKET!
-START > [${startPosition}]
-END > [${endPosition}]
-            `
-        } else if (moveDict[move]) {
-            pathTaken = `
-You Made it in 1 Move!
-Here's your path:
-START > [${startPosition}]
-> [${move}]
-END > [${endPosition}]
-            `
-        }
-        })
-    return pathTaken
+    return resultsArray
 }
-console.log(knightTravails([2,7],[3,5]))
+
+class MoveList {
+    constructor(node) {
+        this.root = node
+    }
+}
+
+class Node {
+    constructor(position, possiblePositions) {
+        this.position = position
+        this.possiblePositions = possiblePositions
+    }
+}
+
+function knightTravails(startPosition, endPosition) {
+    const moveDict = new MoveList(endPosition)
+    return moveDict
+}
+console.log(knightTravails([2,7],[2,6]))
