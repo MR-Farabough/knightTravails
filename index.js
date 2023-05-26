b593aaf33fbf51717d907545078686275ded3544
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-05-25 22:29:07 -0500

    Moved function into main function

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function possibleSquares(intArray) {
-    let resultsArray = []
-    const calculationArray = [
-    [intArray[0] + 1, intArray[1] + 2],
-    [intArray[0] - 1, intArray[1] + 2],
-    [intArray[0] + 1, intArray[1] - 2],
-    [intArray[0] - 1, intArray[1] - 2],
-    [intArray[0] - 2, intArray[1] + 1],
-    [intArray[0] - 2, intArray[1] - 1],
-    [intArray[0] + 2, intArray[1] + 1],
-    [intArray[0] + 2, intArray[1] - 1]]
-    const tempStorage = [...new Set(calculationArray)]
-    checkAvailablePositions()
-    function checkAvailablePositions() {
-        tempStorage.forEach(positionData => {
-            const checkOne = positionData[0] < 9 && positionData[0] > 0
-            const checkTwo = positionData[1] < 9 && positionData[1] > 0
-            if (checkOne && checkTwo) {
-                resultsArray.push(positionData)
-            }
-        });
-    }
-    return resultsArray
-}
-
 function knightTravails(startPosition, endPosition) {
+    function possibleSquares(intArray) {
+        let resultsArray = []
+        const calculationArray = [
+        [intArray[0] + 1, intArray[1] + 2],
+        [intArray[0] - 1, intArray[1] + 2],
+        [intArray[0] + 1, intArray[1] - 2],
+        [intArray[0] - 1, intArray[1] - 2],
+        [intArray[0] - 2, intArray[1] + 1],
+        [intArray[0] - 2, intArray[1] - 1],
+        [intArray[0] + 2, intArray[1] + 1],
+        [intArray[0] + 2, intArray[1] - 1]]
+        const tempStorage = [...new Set(calculationArray)]
+        checkAvailablePositions()
+        function checkAvailablePositions() {
+            tempStorage.forEach(positionData => {
+                const checkOne = positionData[0] < 9 && positionData[0] > 0
+                const checkTwo = positionData[1] < 9 && positionData[1] > 0
+                if (checkOne && checkTwo) {
+                    resultsArray.push(positionData)
+                }
+            });
+        }
+        return resultsArray
+    }
     const startPosMoves = possibleSquares(startPosition)
     const endPosMoves = possibleSquares(endPosition)
     let pathTaken = ''
