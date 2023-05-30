2e74c97668cbde1c7569cdb0d171e569c440eae2
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-05-30 16:34:11 -0500

    Knight Travails

--- index.js
+++ index.js
@@ -1,90 +1,31 @@
-function knightTravails(startPosition, endPosition) {
-    const board = new Map()
-    let path = [startPosition]
-    addNode(startPosition)
-    buildPositions(board.get(startPosition))
-    function addNode(cord) {
-        board.set(cord, probePositions(cord))
-    }
-
-    function probePositions(intArray) {
-        let resultsArray = []
-        const calculationArray = [
-        [intArray[0] + 1, intArray[1] + 2],
-        [intArray[0] - 1, intArray[1] + 2],
-        [intArray[0] + 1, intArray[1] - 2],
-        [intArray[0] - 1, intArray[1] - 2],
-        [intArray[0] - 2, intArray[1] + 1],
-        [intArray[0] - 2, intArray[1] - 1],
-        [intArray[0] + 2, intArray[1] + 1],
-        [intArray[0] + 2, intArray[1] - 1]]
-        const tempStorage = [...new Set(calculationArray)]
-        checkAvailablePositions()
-        function checkAvailablePositions() {
-            tempStorage.forEach(positionData => {
-                const checkOne = positionData[0] < 9 && positionData[0] > 0
-                const checkTwo = positionData[1] < 9 && positionData[1] > 0
-                if (checkOne && checkTwo) {
-                    resultsArray.push(positionData)
-                }
-            });
+function knightMoves(start, end) {
+    const moves = [
+      [1, 2], [2, 1], [2, -1], [1, -2],
+      [-1, -2], [-2, -1], [-2, 1], [-1, 2]
+    ]; 
+    const visited = new Set();
+    const queue = [[start, []]];
+    while (queue.length > 0) {
+      const [currentSquare, path] = queue.shift();
+      if (currentSquare[0] === end[0] && currentSquare[1] === end[1]) {
+        path.push(currentSquare);
+        return `You made it in ${path.length - 1} moves! Here's your path:\n${path.map(square => `  ${square}`).join('\n')}`;
+      }
+      
+      visited.add(`${currentSquare[0]},${currentSquare[1]}`);
+      for (const move of moves) {
+        const nextSquare = [currentSquare[0] + move[0], currentSquare[1] + move[1]];
+        if (isValidSquare(nextSquare) && !visited.has(`${nextSquare[0]},${nextSquare[1]}`)) {
+          queue.push([nextSquare, [...path, currentSquare]]);
         }
-        return resultsArray
+      }
     }
-
-    function buildResults() {
-        let paths = []
-        let moveCount = 0
-        path.forEach(cord => {
-            paths.push(`
-${moveCount} => ${JSON.stringify(cord)}`)
-moveCount++
-        })
-        let resultString = `
-Path Found ${path.length - 1} Move(s)
-${paths}`
-        return console.log(resultString)
-    }
-
-    function checkEnd(cord) {
-        const endCheckOne = cord[0] == endPosition[0]
-        const endChecktwo = cord[1] == endPosition[1]
-        return endCheckOne && endChecktwo ? true : false
-    }
-
-    function buildPositions(key) {
-        key.forEach(nextCord => {
-            addNode(nextCord)
-        })
-    }
-
-    function convertMapToArray() {
-        let queue = []
-        for (const position of board) queue.push(position)
-        return queue
-    }
-    
-    probePositions(startPosition).forEach(cord => {
-        if (checkEnd(cord) && !checkEnd(path[path.length - 1])) {
-            return path.push(cord), buildResults()
-        } else if (checkEnd(startPosition)) {
-            return console.log(`YOU ARE ALREADY AT SPOT ${startPosition}`)
-        }
-    }) 
-    if (path.length < 2) {
-        let temp = []
-    convertMapToArray(board.values()).forEach(positionArray => {
-        positionArray[1].forEach(position => {
-            if (checkEnd(position) && !checkEnd(path[path.length - 1])) {
-                return path.push(positionArray[0], position), buildResults()
-            }
-            temp.push(position)
-        })
-    })
-
-    const totalKnownLocations = [...new Set(temp)]
-    // function for traversing the board again
-    console.log(`${(totalKnownLocations.length/64)*100}% Chance of finding first`)
-    }
-}
-knightTravails([8,4],[6,4])
+    return
+  }
+  
+  function isValidSquare(square) {
+    const [row, col] = square;
+    return row >= 0 && row < 8 && col >= 0 && col < 8;
+  }
+// GG 😭
+console.log(knightMoves([0,0], [7,7]));
